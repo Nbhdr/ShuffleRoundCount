0b8afe7b51bf13a424eb04094efd7f5e74615311
Nbhdr <ibhadralobo@ucdavis.edu>
2016-12-21 17:26:38 -0800

    Update CodeReview.c

--- CodeReview.c
+++ CodeReview.c
@@ -31,7 +31,7 @@ void main( void )                    // 2
 /*
 1. If err code is 0, if(err) will never trigger on a "success -- no error" case since GetErrorString will never be called
 
-2. Double check that returing void from main will work on the target system
+2. Double check that returing void from main will work on the target system 
     
 3.  While the bit represention is the same, I suggest use of '\0' for readibility and purpose. 
     Using strcpy will also copy the '\0' from the string literals automatically.
@@ -44,6 +44,7 @@ void main( void )                    // 2
 6. For readibility and maintenance, I suggest defining case constants, i.e. #define SUCCESS 0   #define OVERFLOW 2
 
 
-7. returning errorString may lead to an error. errorString is local to the function scope and the pointer may no longer point to intended data
-when GetErrorString returns from function stack. Work arounds include passing in an array or making errorString array Static
+7. returning errorString may lead to unwanted behavior or data. errorString is local to the function scope and the pointer may no longer point to intended data
+when GetErrorString returns from function stack. Work arounds include passing in an array or making errorString array Static. Since errorString is not static, 
+it should be explicitly initialized = {0}
 */
